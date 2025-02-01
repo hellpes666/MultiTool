00447d447ea5d8c44b7affa18b7f66d9029ab671
maratdev0210 <maratforwork1@gmail.com>
2025-02-01 22:00:39 +0300

    wrap App inside Helmet

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,22 @@ import { useSelector } from 'react-redux';
 import { RootState } from './store/store';
 import { Header, MainPage } from './components';
 import Container from './components/Container';
+import { HelmetProvider } from 'react-helmet-async';
 
 function App() {
   const darkMode = useSelector((state: RootState) => state.theme.darkMode);
 
   return (
-    <div
-      className={`h-full min-h-dvh w-full bg-[var(--bg-dark)] py-4 dark:bg-[var(--bg-light)] ${darkMode && 'dark'}`}
-    >
-      <Header darkMode={darkMode} />
-      <Container>
-        <MainPage />
-      </Container>
-    </div>
+    <HelmetProvider>
+      <div
+        className={`h-full min-h-dvh w-full bg-[var(--bg-dark)] py-4 dark:bg-[var(--bg-light)] ${darkMode && 'dark'}`}
+      >
+        <Header darkMode={darkMode} />
+        <Container>
+          <MainPage />
+        </Container>
+      </div>
+    </HelmetProvider>
   );
 }
 
