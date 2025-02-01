d373851ecb40d1bb37ae148e2bf89dcf5e3bfc94
maratdev0210 <maratforwork1@gmail.com>
2025-02-01 19:40:45 +0300

    rewrite route type to enum type

--- src/helpers/routes/route.d.ts
+++ src/helpers/routes/route.d.ts
@@ -1,3 +1,7 @@
-type Route = 'Weather' | 'Movies' | 'Blog' | 'Friends';
+// NOTE: this file should be deleted later during the development;
+// NOTE: Route definitions are declared in routes.ts file
+
+/*type Route = 'Weather' | 'Movies' | 'Blog' | 'Friends';
 
 export type { Route };
+*/
--- src/helpers/routes/routes.ts
+++ src/helpers/routes/routes.ts
@@ -1,3 +1,15 @@
-import { Route } from './route';
-//! Change to Object or Enum
-export const ROUTES: Route[] = ['Weather', 'Movies', 'Blog', 'Friends'];
+// store the Route values in enum type
+enum Route {
+  Weather = 'Weather',
+  Movies = 'Movies',
+  Blog = 'Blog',
+  Friends = 'Friends',
+}
+
+// Used in Header
+export const ROUTES: Route[] = [
+  Route.Weather,
+  Route.Movies,
+  Route.Blog,
+  Route.Friends,
+];
