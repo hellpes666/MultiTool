ac818fab76193907b4db7fbb03b28dc3796b6608
maratdev0210 <maratforwork1@gmail.com>
2025-02-01 21:59:55 +0300

    Connect Weather page to Helmet

---
+++ src/components/Weather/Weather.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SEO from '../SEO/Seo';
+
+const Weather: React.FC = () => {
+  return (
+    <div>
+      <SEO title='Weather'></SEO>
+      <h1>Weather App!</h1>
+    </div>
+  );
+};
+
+export default Weather;
